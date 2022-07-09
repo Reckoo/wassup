479222ce864e15f2e62af2b75d92b175c63f9ebd
Reck <gsdt008@gmail.com>
2022-07-09 16:31:16 +0545

    Checking logs`

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ export default function MyApp({
     const router = useRouter();
     const [loading, setLoading] = useState(false);
       console.log("Working");
+      console.log(process.env.NEXTAUTH_URL);
     useEffect(() => {
       router.events.on('routeChangeStart', () => setLoading(true));
       router.events.on('routeChangeComplete', () => setLoading(false));
