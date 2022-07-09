f7adb9714ff0efa20f72787ff7837fbd4443485f
Reck <gsdt008@gmail.com>
2022-07-09 19:27:20 +0545

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./components/**/*.{js,ts,jsx,tsx}',
-  './pages/**/*.{js,ts,jsx,tsx}'],
+  content: ["./components/**/*.{js,ts,jsx,tsx}",
+  './pages/**/*.{js,ts,jsx,tsx}',"./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
